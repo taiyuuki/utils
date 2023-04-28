f5566425896e42b72d1efbddc42f536f77d66d5d
taiyuuki <taiyuuki@qq.com>
2023-04-28 18:12:24 +0800

    feat: console log error

--- src/modules/error.ts
+++ src/modules/error.ts
@@ -7,3 +7,12 @@
 export function throwTypeError(type: string, name: string) {
   throw new TypeError(`Expected a ${type} as ${name}.`)
 }
+
+/**
+ * 控制台打印类型错误
+ * @param type - 正确的类型
+ * @param name - 变量名
+ */
+export function logTypeError(type: string, name: string) {
+  console.error(`Expected a ${type} as ${name}.`)
+}
